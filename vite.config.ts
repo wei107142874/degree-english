594f807291b23d647cf0fa12ab4782718a2f4385
yong.wei <1071427874@qq.com>
2026-08-16 00:35:06 +0800

    修复 PWA 图标路径

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
         start_url: '/',
         lang: 'zh-CN',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/degree-english/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/degree-english/icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       },
       workbox: {
